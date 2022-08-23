50c29bde09799900c2cc187e0c917cb65bd079b0
tamene gelaye <tamenes21@gmail.com>
2022-08-23 10:34:41 +0000

    number checking

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/user/bin/python3
+
+import random
+
+number = randint(-10, 10)
+
+if number > 0:
+	print(number, "is positive")
+elif number == 0:
+	print(number,"is zero")
+elif number < 0:
+	print(number, "is negative")

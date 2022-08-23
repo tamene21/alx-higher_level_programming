7064b04ad7499bac23c38793bb9d4b3094c06bbb
Tamene Gelaye <tamenes21@gmail.com>
2022-08-23 11:45:11 -0700

    lower case

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def isLower(c):
-    if (ord(c) >= ord('a') and ord(c) <= ord('z')):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
     else:
         return False     

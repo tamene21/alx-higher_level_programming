e4ae8c43421019e084f2a2795ab37bc35e08efec
Tamene Gelaye <tamenes21@gmail.com>
2022-08-23 11:35:51 -0700

    first commit

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def isLower(c):
+    counter = 0
+
+    for char in x:
+        if (ord(char) >= 97 and ord(char) <= 122):
+            return True
+        else:
+            return False     

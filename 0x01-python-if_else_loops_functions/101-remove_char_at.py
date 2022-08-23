c7de11abf870ce5d6c78f1a42e84154f43fbef05
Tamene Gelaye <tamenes21@gmail.com>
2022-08-23 15:04:18 -0700

    edited

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -6,12 +6,3 @@ def remove_char_at(str, n):
         return aux
     else:
         return str
-
-
-remove_char_at = __import__('101-remove_char_at').remove_char_at
-
-print(remove_char_at("Chicago", 3))
-print(remove_char_at("Chicago", 15))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))

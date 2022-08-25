85bdffb73b74684c7fc6ef9d304cc0eabbbc1e7b
Tamene Gelaye <tamenes21@gmail.com>
2022-08-25 05:28:29 -0700

    requirment

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if num == 1:
         print("{} arguments.".format(num - 1))
     elif num == 2:
-        print("{} argument.".format(num - 1))
+        print("{} argument:".format(num - 1))
     else:
-        print("{} arguments.".format(num - 1))
+        print("{} argument:".format(num - 1))
     
     for i in range(1, num):
         print("{}: {}".format(i, sys.argv[i]))

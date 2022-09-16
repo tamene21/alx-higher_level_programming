4b7dd5054ba207bf46249010647720d5d4c83487
Tamene Gelaye <tamenes21@gmail.com>
2022-09-16 01:54:15 -0700

    correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    '''Represents a square.
-    private instance attribute: size
+    """Represents a square.
+    private instance attribute: size.
     instantiation with size (no type /value verification).
-    '''
+    """
 
     def __init__(self, size):
-        '''initializes the data'''
+        """initializes the data"""
         self.__size = size

bc09e709eda8bee401cb2dc7fd2bc98cf0f2845d
Tamene Gelaye <tamenes21@gmail.com>
2022-09-21 04:21:14 -0700

    edited

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,3 +6,15 @@ class Rectangle:
 """
 class that creat a rectangle
 """
+def __init__(self, width=0, height=0):
+    """Constructor initialize the class rectangle
+    Keyword Arguments:
+       width {int} -- width of the rectangle (default: {0})
+       height {int} -- height of the rectangle (default: {0})
+       """
+    self.width = width
+    self.height = height
+
+    @property
+def width(self):
+

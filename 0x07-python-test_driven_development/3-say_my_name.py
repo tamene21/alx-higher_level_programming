ee225ed374bdd5bf384514ed729fd357e9c61fb5
Tamene Gelaye <tamenes21@gmail.com>
2022-09-23 14:36:10 -0700

    new commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,45 +1,18 @@
 #!/usr/bin/python3
 """
-    matrix division module
+    Module containing ``say_my_name``` function.
 """
 
 
-def matrix_divided(matrix, div):
-    """ Takes a matrix and divides the values by 'div'.
+def say_my_name(first_name, last_name=""):
+    """ Function prints `first_name` and `last_name`
     Args:
-        matrix (:obj:'list' of :obj:'list'): lists of lists of integers/floats.
-        div (int or float): The divisor.
+        first_name (string): First name.
+        last_name (string): Last name.
     """
-    row_len = -1
-    if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
-    if div is 0:
-        raise ZeroDivisionError("division by zero")
 
-    if type(matrix) is not list or len(matrix) is 0:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/"
-                        "floats")
-
-    new_matrix = []
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of "
-                            "integers/floats")
-        if row_len is -1:
-            row_len = len(row)
-            if row_len is 0:
-                raise TypeError("matrix must be a matrix (list of lists) of "
-                                "integers/floats")
-        else:
-            if row_len is not len(row):
-                raise TypeError("Each row of the matrix must have the same "
-                                "size")
-        new_row = []
-        for ele in row:
-            if type(ele) is int or type(ele) is float:
-                new_row.append(round(ele / div, 2))
-            else:
-                raise TypeError("matrix must be a matrix (list of lists) of "
-                                "integers/floats")
-        new_matrix.append(new_row)
-    return new_matrix
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {:s} {:s}".format(first_name, last_name))

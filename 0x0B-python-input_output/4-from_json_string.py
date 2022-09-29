1d286240828363345e60845a63ea6bdc14749764
Tamene Gelaye <tamenes21@gmail.com>
2022-09-29 03:31:58 -0700

    new commit

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 4-JSON string to object
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    changing from json to string
+
+    Args:
+    - my_str: a json file
+
+    Return:
+    Returns a string file
+    """
+    return json.load(my_str)

3e2c8bebf40cf3f844b6e45bf902135617889f6e
Tamene Gelaye <tamenes21@gmail.com>
2022-09-29 12:11:25 -0700

    task 12 added

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" defines a class MyInt that inherits from int
+    and overloads the == and != operators.
+"""
+
+
+class MyInt(int):
+    """overloads the == and != operators """
+
+    def __eq__(self, num):
+        """ return False if equal"""
+        return self.real != num
+
+    def __ne__(self, num):
+        """ return True if equal"""
+        return self.real == num

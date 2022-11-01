749a814e7c9871df945cc30da4b91fef6c4f269f
tamene gelaye <tamenes21@gmail.com>
2022-11-01 06:37:23 -0700

    edited

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- listing the records
-SELECT sore, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;

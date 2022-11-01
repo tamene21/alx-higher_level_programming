9aae235bd68f1ac16e54137a046f78559cfd59ac
tamene gelaye <tamenes21@gmail.com>
2022-11-01 05:51:43 -0700

    new commit

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- creating a full table 
+CREATE TABLE IF NOT EXISTS table_table (id INT,
+	name VARCHAR(256),
+	score INT) VALUES (
+	(1,2,3,4), 
+	('John','Alex', 'Bob','George'), 
+	(10, 3, 14, 8));

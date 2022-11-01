b0cbea2b1540424c24b949f7a978f2e2e4c8a02f
tamene gelaye <tamenes21@gmail.com>
2022-11-01 06:00:06 -0700

    multiple lines added

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,8 @@
 -- creating a full table 
 CREATE TABLE IF NOT EXISTS second_table (id INT,
 	name VARCHAR(256),
-	score INT) VALUES (
-	(1,2,3,4), 
-	('John','Alex', 'Bob','George'), 
-	(10, 3, 14, 8));
+	score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);

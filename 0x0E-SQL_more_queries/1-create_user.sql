2f46b6868026855c84e58b1967e1a7ec059a3470
tamene gelaye <tamenes21@gmail.com>
2022-11-04 00:36:30 -0700

    edited

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- a scripit theat creats mysql server
-IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES 
-ON *.* TO 'user_0d_1'@'localhost';
+-- a scripit create a user with all privilages
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

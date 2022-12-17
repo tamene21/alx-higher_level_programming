81135e39213f2c090f17e142a5c6a9fd8373baf2
Tamene Gelaye <tamenes21@gmail.com>
2022-12-16 18:39:25 -0800

    modified

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
-
 import urllib.request
 
 def fetcher():
+
     """fetcher"""
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
@@ -11,6 +11,7 @@ def fetcher():
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
 
 if __name__ == "__main__":
     fetcher()

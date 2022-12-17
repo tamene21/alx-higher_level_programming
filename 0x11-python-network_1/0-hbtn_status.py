2af3680e2ed53a2519d0140e3d32f2cee3e5f0a9
Tamene Gelaye <tamenes21@gmail.com>
2022-12-16 18:23:54 -0800

    modified

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,5 +3,10 @@
 
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    html = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))

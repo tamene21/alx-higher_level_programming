0fd16ffc451906b52db7b4147a2ddb4b719fc24c
Tamene Gelaye <tamenes21@gmail.com>
2022-12-16 18:32:49 -0800

    modififed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,14 @@
 
 import urllib.request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
+if __name__ == "__main__":
+    fetcher()

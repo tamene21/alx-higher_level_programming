afbbf5a625ca1b48ce61112cd42adbf210bc0c38
Tamene Gelaye <tamenes21@gmail.com>
2023-03-14 14:02:47 +0000

    creating instance in a class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let S = '';
+      for (let j = 0; j < this.width; j++) { S += 'X'; }
+    }
+  }
+}

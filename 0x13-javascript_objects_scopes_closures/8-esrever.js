60e081518d256b65d564ca14ef4ffe461a1ab58d
Tamene Gelaye <tamenes21@gmail.com>
2023-03-14 19:48:47 +0000

    variable key word change

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let myList = [];
+  const myList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    myList.push(list[i]); 
+    myList.push(list[i]);
   }
   return myList;
 };

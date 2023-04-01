d3d374e23c3c2a946af0b8609f53df583a9eb976
Tamene Gelaye <tamenes21@gmail.com>
2023-03-31 22:17:45 -0700

    urllib liberary import correclected as urllib.request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
-from urllib import request
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         response = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(response)))

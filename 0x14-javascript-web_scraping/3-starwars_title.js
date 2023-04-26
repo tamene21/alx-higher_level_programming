0f5421617eea802ce040fe289c8a3f66083e07c1
Tamene Gelaye <tamenes21@gmail.com>
2023-04-26 09:43:04 +0000

    semistandard rule and else conditon coorected

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,13 +2,12 @@
 // print a matched starware movie title and episode number
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/:id';
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 
 request(url + process.argv[2], function (error, res, body) {
   if (error) {
     console.log(error);
-  } else {
-    const json = JSON.parse(body);
-    console.log(json.title);
   }
+  const json = JSON.parse(body);
+  console.log(json.title);
 });

802a7b27e2c75fb47a9f16e5544d2deada99175b
Tamene Gelaye <tamenes21@gmail.com>
2023-04-26 10:47:59 +0000

    unused variable removed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,6 @@
 // Display the status of  GET Request
 
 const request = require('request');
-const url = process.argv[2];
 
 request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
